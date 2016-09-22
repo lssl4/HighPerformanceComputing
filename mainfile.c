b0579e0e9de7ab5619b5172f83ec6c3441aadbd6
Uni <shaunleong@outlook.com>
2016-09-22 14:23:41 +0800

    Fixed keys.txt input

--- mainfile.c
+++ mainfile.c
@@ -6,30 +6,31 @@ int main(){
 
 //Reading in the files
 FILE *fp = fopen("keys.txt", "r");
- char buffer[1024] ;
- char *record,*line;
+ //char buffer[1024] ;
+ //char *record;
+ //char *line;
  int R=0;
- int mat[100][100];
-long keys[4400];
+ //int mat[100][100];
+long long keys[4400];
 
 if(fp == NULL){
-	printf("\nFile opening failed\n");
+	printf("File opening failed\n");
 	return -1;
 }
 
+while(fscanf(fp, "%lli",&keys[R++])>0);
 
-while((line = fgets(buffer,sizeof(buffer), fp) ) != NULL){
-
-	record = strtok(line, "\n");
-	keys[R++] = atol(record);
+	 
+	
+	 
 	
 
 
-}
 
-for(int j = 0 ; j < R; j ++){
 
-	printf("%lld\n",keys[j]);
+for(int j = 0 ; j < sizeof(keys)/sizeof(long long); j ++){
+
+	printf("%lli\n",keys[j]);
 }
 
 }
