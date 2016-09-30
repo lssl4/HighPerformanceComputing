60d549af68bb7eacabb0045e1f4913546e3f7e4f
KuroKenshi <smasher1337@hotmail.com>
2016-09-30 17:23:39 +0800

    think have neighbours done, have slight issues with the output, now need to implement into the whole project

--- neigbours.cpp
+++ neigbours.cpp
@@ -4,122 +4,61 @@
 #include <string.h>
 #include <stdlib.h>
 #include <cmath>
-#define dia 0.000001
+#define dia 0.000002
 
 using namespace std;
 #include <unordered_set>
 #include <iostream>
 #include <vector>
-#include <algorithm>
+//#include <algorithm>
 #include "type.h"
-
+// #include <fopenmp.h>
 double element[7]={0.000452,0.000453,0.000454,0.000455,0.000456,0.000458,0.000459};
-int pivot;
-
-
 
 void getNeighbour(double *s){
     vector<double> v1;
     vector<double> v2;
     vector<double>::iterator it;
-    // it = v1.begin();
-
-    //int start = 0;
     int j=1;
-    int pivot=-1;
-    //int x = 0;
-    //while(x<(sizeof(element)/sizeof(element[0]))-1)// this while loop controls the location of x(ie the start values)
-    //j=x;
-    for (int start = 0; start < (sizeof(element)/sizeof(element[0]))-1; start++)
+    int pivot=0;
+
+    v1.push_back(element[0]);
+    for (int start = 0; start<(sizeof(element)/sizeof(element[0])); start++)
     {
         //element j= the element being checked to see if it is within dia distance.
-        // v1.push_back(element[i]);
-        while(j<(sizeof(element)/sizeof(element[0]))){
-            //for(start;start<(sizeof(element)/sizeof(element[0]));start++){
-            if((1000000.0*element[j])-(1000000.0*element[start]) <= (1000000.0*dia)){
+
+        while(j<sizeof(element)/sizeof(element[0])&&(1000000.0*element[j])-(1000000.0*element[start]) <= (1000000.0*dia)){
+
 
                 v1.push_back(element[j]);
-                pivot=j;
-                j++;
+                pivot++;//increase the pivot index, note the pivot index starts from 0(same as arrays)
+            j++;
+
             }
-            else{
-                j=start;
 
-                for( it = v1.begin();it!=v1.end();it++)
-                {
-                    cout<< *it <<endl;
-                }
-                //v1.erase(v1.begin());
-                //cout<< pivot <<endl;
-                //send vector out, 
-                //cout<< j <<endl;
+        //send the vector and pivot to makeBlocks
 
-                break;
-            };
+
+
+
+        //for debugging
+        for( it = v1.begin();it!=v1.end();it++)
+        {
+            cout<< *it <<endl;
+        }
+        cout<< pivot <<endl;
+
+
+        v1.erase(v1.begin());//remove front element
+        pivot = 0;//reset pivot index point
         }
 
-        //x++;
-        //start=x;
-        //j=x;
-        //cout<<  <<endl;
     }
-    //for(int j=start;j<sizeof(element)/sizeof(element[0]);j++){
-    /*if( (1000000.0*element[j])-(1000000.0*element[start]) <= (1000000.0*dia)){
-        v1.push_back(element[j]);//add element end of vector, that is within dia distance
-        pivot=j;//the last element of the vector
-    }
-    //genBlocks(vector<ELEMENT> v1, int pivot)
-    //find pivot. pivot of first neighbourhood will be last element of first neighbour
-    //
-    //
 
-}
-
-}}
-//}
-
-cout<<"Stored values are:"<<endl;
-for(iterator = set.begin();iterator!=set.end();iterator++)sd
-{
-cout<< *iterator <<endl;
-}*/
-    // cout<< pivot <<endl;/*
-    /*for(it = v1.begin();it!=v1.end();it++)
-   {
-     cout<< *it <<endl;
-   }*/
-
-}
 
 
 
 int main(){
-    //vector<double> v1;
     getNeighbour(element);
 }
 
-/*
-#include <iostream>
-#include <unordered_set>
- 
-using namespace std; 
-int main()
-{
-    unordered_set<int> set;
-        
-    set.insert(5);    
-    //Insert same items here to see if more than one of them
-    //have been inserted
-    set.insert(8);
-    set.insert(8);    
-    set.insert(45);
-    set.insert(50);    
-    
-    //iterate the set here
-    unordered_set<int>::iterator iterator;    
-    cout<<"Stored values are:"<<endl;
-    for(iterator = set.begin();iterator!=set.end();iterator++)
-    {
-      cout<< *iterator <<endl;
-    }
-}*/
