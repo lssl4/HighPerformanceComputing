3b4bb84dff6a5836295b3a4b06ed9d501e3b41e7
Uni <shaunleong@outlook.com>
2016-10-01 19:11:29 +0800

    Poor attempt to remove redundant neighborhoods.

--- shaunneighbors.cpp
+++ shaunneighbors.cpp
@@ -12,12 +12,9 @@ using namespace std;
 #include <unordered_set>
 #include <iostream>
 #include <vector>
-//#include <algorithm>
 #include "type.h"
 
 vector<double> sample = {0.0004520,0.0004525, 0.0004531, 0.0004532, 0.0004533, 0.0004534, 0.0004535, 0.0004535,0.000454,0.000454,0.000458,0.000459, 0.00046, 0.0004601, 0.0004602, 0.0004603, 0.0004604};
-
-
 
 
 bool lowHigh (ELEMENT i, ELEMENT j) { 
@@ -25,43 +22,45 @@ bool lowHigh (ELEMENT i, ELEMENT j) {
 }
 
     
-vector<vector<ELEMENT>> getNeighbours(vector<ELEMENT> s){
+    vector<vector<ELEMENT>> getNeighbours(vector<ELEMENT> s){
     
     vector<vector<ELEMENT>> listOfNeigh;
-    int prevLastIndex =-1;
+    int prevLastIndex = 0;
 
     //sort s
     sort(s.begin(), s.end(), lowHigh);
 
-
-
-    for (int i = 0; i < s.size(); ++i)
-    {
-
-        /* code */
-       printf("%.7f ", s[i]);
-    }
-    cout<< endl << endl;
 
     //for each s in the array, find its neighborhood, including the last s even though there's no more trailing elements after it. 
     for (int start = 0; start < s.size(); start++)
 
     {
         vector<ELEMENT> v1;
-        vector<ELEMENT> v2;
+       
         int pivot = 1;
 
-        v1.push_back(s[start]);
+        //v1.push_back(s[start]);
 
+        printf("Just before insert Start: %i prevLastIndex: %i\n", start, prevLastIndex);
+
+        v1.insert(v1.end(), s.begin() + start,  s.begin() + prevLastIndex );
+
+        cout << "v1 after start: " <<start << endl;
+        for (int i = 0; i < v1.size(); ++i)
+        {
+            printf("%.7f ", v1[i].datum);
+        }
+
+        cout << endl << endl;
 
         //for each trailing s after start, check if it's within neighborhood. vectorsize -2 
         //because of pivot number at end and shifting everything to the right by previous list size
-        for(int j = start +1 ;j < s.size() ; j++){
+        for(int j = prevLastIndex; j <= s.size() ; j++){
           
             printf("j: %i start: %i\n", j, start);
             printf("%.12f - %.12f = %.12f | %i\n\n",(1000000.0*s[j].datum), (1000000.0*s[start].datum), (1000000.0*s[j].datum)-(1000000.0*s[start].datum)  , (1000000.0*s[j].datum)  - (1000000.0*s[start].datum) < (1000000.0*dia) );
 
-            if( ( (1000000.0*s[j].datum)-(1000000.0*s[start].datum) )< (1000000.0*dia) ){
+            if( j <s.size() &&( (1000000.0*s[j].datum)-(1000000.0*s[start].datum) )< (1000000.0*dia) ){
 
 
                 v1.push_back(s[j]);
@@ -71,32 +70,24 @@ vector<vector<ELEMENT>> getNeighbours(vector<ELEMENT> s){
                 
             }
 
-            //if s j is not within the dia distance
+            //if j is not within the dia distance, break and add the vector so far to the list
             else{
 
-                /*if(v1.size() < 4){
-                    break;
-                }*/
-
-
                 
+                //update prevLastIndex with jth element that is just right out of bounds
+                prevLastIndex = j;
+                cout << "prevLastIndex: " << prevLastIndex <<endl;
                 break;
             }
-
-            
 
 
 
         }
 
-        
-                  //if there's no more elements to be added, get pivot if any
+                  //if there's no more elements to be added, append pivot to the vector
                 if(listOfNeigh.size() >0){
 
-                    vector<ELEMENT> prevVector = listOfNeigh.back();
-
-                 
-
+                     vector<ELEMENT> prevVector = listOfNeigh.back();
                     int prevVectRow = prevVector[prevVector.size()-2].row;
                     int currVectRow = v1.front().row;
 
@@ -104,6 +95,8 @@ vector<vector<ELEMENT>> getNeighbours(vector<ELEMENT> s){
                     if(prevVectRow - currVectRow >=0 ){
                         pivot = prevVectRow - currVectRow +1;
                         printf("pivot: %i\n", pivot);
+                    }else{
+                         pivot = v1.size();
                     }
 
                         
@@ -111,8 +104,6 @@ vector<vector<ELEMENT>> getNeighbours(vector<ELEMENT> s){
                 }else{
                     pivot = v1.size();
                 }
-
-
 
 
                     //push back the pivot number in the neighborhood vector as an element. pivot appears at the end of vector
@@ -125,10 +116,14 @@ vector<vector<ELEMENT>> getNeighbours(vector<ELEMENT> s){
                     listOfNeigh.push_back(v1);
 
 
+                    printf("Just pushed in v1\n");
 
-        
-
-     
+                    for (int x = 0; x < v1.size(); ++x)
+                    {
+                        /* code */
+                        printf("%.7f \n", v1[x].datum);
+                    }
+                    cout << endl;
     }
 
     
@@ -157,17 +152,7 @@ int main(){
 
     }
 
-
-
-    //vector<double> v1;
     vector<vector<ELEMENT>> output = getNeighbours(element);
-
-        /*for( vector<vector<double>>::iterator it = output.begin();it!=output.end();it++)
-                {
-                    for(vector<double>::iterator it2 = (*it).begin(); it2 != (*it).end(); it2++)
-                            {cout<< *it2 << " ";}
-                        cout << endl;
-                }*/
 
     for(int i = 0; i < output.size(); i++){
         vector<ELEMENT> neigh = output[i];
