3b692458b2c126e99fd9060b67b22185a0d7ccc5
Tzi Siong Leong <20753794@ecm.uwa.edu.au>
2016-11-06 15:39:21 +0800

    Modified host files and runSch.sh script

--- runSch.sh
+++ runSch.sh
@@ -1,3 +1,5 @@
-#PBS -l nodes=29:ppn=12
+#PBS -l nodes=20:ppn=5
+#PBS -m abe
+#PBS -M 20753794@student.uwa.edu.au
 source /etc/bash.bashrc
-mpirun example
+mpirun /home/uniwa/students4/students/20753794/linux/CITS3402/mainfile2OMP data.txt keys.txt 4400 500 0.000001
